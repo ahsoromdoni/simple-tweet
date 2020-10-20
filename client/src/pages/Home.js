9b10b66310530f5342e8309506a5275b17f3d275
Ahmad Solehudin Romdoni <ahsoromdoni@gmail.com>
2020-10-20 23:33:15 +0700

    Make query with graphql using apollo-hooks

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
 function Home() {
+    const { loading, data } = useQuery(FETCH_POST_QUERY);
+
+    if (data) {
+        console.log(data)
+    }
     return (
         <div>
             <h1>Home</h1>
         </div>
     );
 }
+
+const FETCH_POST_QUERY = gql`
+    {
+        getPosts{
+            id
+            body
+            username
+            comments{
+                id
+                body
+                username
+                createdAt
+            }
+            likes{
+                username
+            }
+            likeCount
+            commentCount
+            createdAt
+        }
+    }
+`
 
 export default Home;
