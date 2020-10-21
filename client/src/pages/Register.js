f40ce171a8405a65230451daeee7d0038613e7b2
Ahmad Solehudin Romdoni <ahsoromdoni@gmail.com>
2020-10-22 00:41:27 +0700

    Fix register using custome hook useForm

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,38 +3,31 @@ import { Form, Button } from 'semantic-ui-react';
 import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
+import { useForm } from '../util/hooks';
+
 function Register(props) {
     const [errors, setErrors] = useState({});
-    const [values, setValues] = useState({
+
+    const { onChange, onSubmit, values } = useForm(registerUserCallback, {
         username: '',
         email: '',
         password: '',
         confirmPassword: ''
-    });
-
-    const onChange = (e) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value
-        })
-    };
+    })
 
     const [addUser, { loading }] = useMutation(REGISTER_USER, {
         update(_, result) {
-            console.log(result);
             props.history.push('/');
         },
         onError(err) {
-            console.log(err.graphQLErrors[0].extensions.exception.errors)
             setErrors(err.graphQLErrors[0].extensions.exception.errors);
         },
         variables: values
     });
 
-    const onSubmit = (e) => {
-        e.preventDefault();
+    function registerUserCallback() {
         addUser();
-    };
+    }
 
     return (
         <div className="form-container">
