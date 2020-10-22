f0d8d237ecd8c44f8161fec8bd6eebfa0f5195ff
Ahmad Solehudin Romdoni <ahsoromdoni@gmail.com>
2020-10-22 17:56:23 +0700

    Login & register implementing context(globalState)

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react';
+import { AuthProvider } from './context/auth';
 
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
@@ -12,14 +13,16 @@ import MenuBar from './components/MenuBar';
 
 function App() {
   return (
-    <Router>
-      <Container>
-        <MenuBar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-      </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
